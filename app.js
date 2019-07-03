7e5d2fdb34af6b469b4baf4eb9542c9666aef0b0
esa-kateryna <esa_kateryna@ukr.net>
2019-07-03 18:37:19 +0300

    задача 6

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function multiply() {
   if (arguments === undefined) {
     return 0;
   } else {
-    for (var i = 0; i < arguments.length; i++) s *= arguments[i];
+    for (let i = 0; i < arguments.length; i++) s *= arguments[i];
     return s;
   }
 }
@@ -21,3 +21,37 @@ function reverse() {
 }
 
 console.log(reverse());
+
+//Задача 3
+function getCodeStr() {
+  let text = "Kate";
+  let convertedText = '';
+  for (var i = 0; i < text.length; i++) {
+    convertedText = convertedText + text.charCodeAt(i) + ' ';
+  }
+  return convertedText;
+}
+
+console.log(getCodeStr());
+
+//задача 4
+//вообще не понимаю как ее сделать
+
+//задача 5
+function getArray() {
+    let n = 10;
+    for (let i = 0; i < n.length; i++) 
+      return getArray[i];
+}
+
+//Задача 6
+function doubleArray() {
+  let arr = [1, 2, 3];
+  let double = [];
+  return double = arr + ',' + arr;
+}
+
+console.log(doubleArray());
+//Задача 7
+
+//Задача 8
