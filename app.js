ba09579f36fb4c4904fc31a17c2cefe81feb6f62
esa-kateryna <esa_kateryna@ukr.net>
2019-07-03 08:28:20 +0300

    задача 1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//Problem 1
-function multiply(arguments) {
+//Задача 1
+function multiply() {
+    var s = 1;
     if (arguments === undefined) {
         return 0;
     } else {
-
+        for(var i=0; i<arguments.length; i++) s *=arguments[i];
+        return s
     }
 }
-
