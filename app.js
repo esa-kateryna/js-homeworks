35963cfba5faf7e18247aa5046abe6bda175a85e
esa-kateryna <esa_kateryna@ukr.net>
2019-07-03 21:08:00 +0300

    задача 7

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ console.log(reverse());
 //Задача 3
 function getCodeStr() {
   let text = "Kate";
-  let convertedText = '';
-  for (var i = 0; i < text.length; i++) {
-    convertedText = convertedText + text.charCodeAt(i) + ' ';
+  let convertedText = "";
+  for (let i = 0; i < text.length; i++) {
+    convertedText = convertedText + text.charCodeAt(i) + " ";
   }
   return convertedText;
 }
@@ -39,19 +39,35 @@ console.log(getCodeStr());
 
 //задача 5
 function getArray() {
-    let n = 10;
-    for (let i = 0; i < n.length; i++) 
-      return getArray[i];
+  let n = 8;
+  let a = [];
+  for (let i = 0; i < n; i++) {
+    a.push(n[i]);
+  }
+  return a;
 }
+
+console.log(getArray());
 
 //Задача 6
 function doubleArray() {
   let arr = [1, 2, 3];
   let double = [];
-  return double = arr + ',' + arr;
+  return (double = arr + "," + arr);
 }
 
 console.log(doubleArray());
-//Задача 7
 
+//Задача 7
+function changeCollection() {
+  let result = arguments[0].splice(1, 1);
+
+  for (i = 1; i < arguments[0].length; i++) {
+    result = result.concat(arguments[0].splice(1, 1));
+  }
+
+  return result;
+}
+
+console.log(changeCollection([3, 4, 5]));
 //Задача 8
