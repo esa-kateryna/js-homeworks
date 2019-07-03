aab4052deae8b89803ad9b8ba7ea7b7673d450b1
esa-kateryna <esa_kateryna@ukr.net>
2019-07-03 10:07:15 +0300

    задача 2

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //Задача 1
 function multiply() {
-    var s = 1;
-    if (arguments === undefined) {
-        return 0;
-    } else {
-        for(var i=0; i<arguments.length; i++) s *=arguments[i];
-        return s
-    }
+  let s = 1;
+  if (arguments === undefined) {
+    return 0;
+  } else {
+    for (var i = 0; i < arguments.length; i++) s *= arguments[i];
+    return s;
+  }
 }
+
+console.log(multiply());
+
+//Задача 2
+function reverse() {
+  let str = "abc";
+  return (str = str
+    .split("")
+    .reverse()
+    .join(""));
+}
+
+console.log(reverse());
